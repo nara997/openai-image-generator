63aeace9667a5bcac2b95f723bcc552a08be3262
nara997 <124362125+nara997@users.noreply.github.com>
2023-03-20 16:19:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,8 +10,11 @@ function App() {
     "Search Anything Here .."
   );
   
+  const api_Key = process.env.API_KEY;
+
+  
   const configuration = useMemo(() => new Configuration({
-    apiKey: process.env.API_KEY,
+    apiKey: api_Key
   }), []);
 
   const openai = useMemo(() => new OpenAIApi(configuration), [configuration]);
