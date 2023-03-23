5a1cf7142d7b550127331e11e3bc1d22433b37c5
nara997 <124362125+nara997@users.noreply.github.com>
2023-03-23 11:54:27 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState, useCallback, useMemo } from "react";
-
+import { Configuration, OpenAIApi } from "openai";
 import "./App.css";
 
 function App() {
@@ -10,42 +10,31 @@ function App() {
     "Search Anything Here .."
   );
   
-  const API_KEY = "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"
  
-  
-  console.log(API_KEY)
 
-  async function myFetch(prompt, configuration) {
-    const response = await fetch("https://api.openai.com/v1/images/generations", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": `Bearer ${API_KEY}`,
-      },
-      body: JSON.stringify({
-        prompt: prompt,
-        n: 1,
-        size: "512x512",
-      }),
-    });
-    const json = await response.json();
-    if (!response.ok) {
-      throw new Error(json.error.message);
-    }
-    return json.data[0].url;
-  }
+  
+const configuration = useMemo(() => new Configuration({
+  apiKey: process.env.API_KEY,
+}), [process.env.API_KEY]);
+  
+  console.log(process.env.API_KEY)
+
+  const openai = useMemo(() => new OpenAIApi(configuration), [configuration]);
 
   const generateImage = useCallback(async () => {
     setPlaceholder(`Recent Search: ${prompt}`);
     setImgSrc("")
     setLoading(true);
-    try {
-      const url = await myFetch(prompt, configuration);
-      setImgSrc(url);
-    } catch (error) {
-      console.error(error);
-    }
-  }, [prompt, configuration]);
+    const response = await openai.createImage({
+      prompt: prompt,
+      n: 1,
+      size: "512x512",
+    });
+    
+    setImgSrc(response.data.data[0].url);
+  }, [prompt, openai]);
+
+  
 
   return (
     <div className="app-main">
